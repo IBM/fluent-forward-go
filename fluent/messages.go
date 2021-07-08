4a5afba4caffce98feabbc95c258c91a21d8779d
Sandy Cash <lhcash@us.ibm.com>
2021-07-08 15:25:40 -0400

    Add support for more of the handshake messages, digest validations

--- fluent/messages.go
+++ fluent/messages.go
@@ -1,6 +1,12 @@
 package fluent
 
 import (
+	"bytes"
+	"crypto/sha512"
+	"encoding/hex"
+	"errors"
+	"io"
+
 	"github.com/vmihailenco/msgpack/v5"
 )
 
@@ -10,10 +16,15 @@ const (
 	MSGTYPE_PONG = "PONG"
 )
 
-// NewHelo returns a msgpack-encoded Helo message with the specified options.
+// Remember that the handshake flow is like this:
+// 1. Server -> HELO -> Client
+// 2. Client -> PING -> Server
+// 3. Server -> PONG -> Client
+
+// PackedHelo returns a msgpack-encoded Helo message with the specified options.
 // if opts is nil, then a nonce is generated, auth is left empty, and
 // keepalive is true.
-func NewHelo(opts *HeloOpts) ([]byte, error) {
+func PackedHelo(opts *HeloOpts) ([]byte, error) {
 	h := Helo{MessageType: MSGTYPE_HELO}
 	if opts == nil {
 		h.Options = &HeloOpts{
@@ -23,6 +34,8 @@ func NewHelo(opts *HeloOpts) ([]byte, error) {
 	return msgpack.Marshal(h)
 }
 
+// Helo is the initial handshake message, sent by the server and received
+// by the client.  Client will respond with a Ping.
 type Helo struct {
 	_msgpack    struct{} `msgpack:",as_array"`
 	MessageType string
@@ -30,7 +43,97 @@ type Helo struct {
 }
 
 type HeloOpts struct {
-	Nonce     string `msgpack:"nonce"`
-	Auth      string `msgpack:"auth"`
+	Nonce     []byte `msgpack:"nonce"`
+	Auth      []byte `msgpack:"auth"`
 	Keepalive bool   `msgpack:"keepalive"`
+}
+
+// PackedPing returns a msgpack-encoded PING message.  The digest is computed
+// from the hostname, key, salt, and nonce using SHA512.
+func PackedPing(hostname string, sharedKey, salt, nonce []byte, username, password string) ([]byte, error) {
+	p := Ping{
+		MessageType:        MSGTYPE_PING,
+		ClientHostname:     hostname,
+		SharedKeySalt:      salt,
+		SharedKeyHexDigest: computeHexDigest(salt, hostname, nonce, sharedKey),
+		Username:           username,
+		Password:           password,
+	}
+	return msgpack.Marshal(p)
+}
+
+// Ping is the response message sent by the client after receiving a
+// Helo from the server.  Server will respond with a Pong.
+type Ping struct {
+	_msgpack           struct{} `msgpack:",as_array"`
+	MessageType        string
+	ClientHostname     string
+	SharedKeySalt      []byte
+	SharedKeyHexDigest []byte
+	Username           string
+	Password           string
+}
+
+// PackedPong returns a msgpack-encoded PONG message.  AuthResult indicates
+// whether the credentials presented by the client were accepted and therefore
+// whether the client can continue using the connection, switching from
+// handshake mode to sending events.
+// As with the PING, the digest is computed
+// from the hostname, key, salt, and nonce using SHA512.
+// Server implementations must use the nonce created for the initial Helo and
+// the salt sent by the client in the Ping.
+func PackedPong(authResult bool, reason string, hostname string, sharedKey, salt, nonce []byte) ([]byte, error) {
+	p := Pong{
+		MessageType:        MSGTYPE_PONG,
+		AuthResult:         authResult,
+		Reason:             reason,
+		ServerHostname:     hostname,
+		SharedKeyHexDigest: computeHexDigest(salt, hostname, nonce, sharedKey),
+	}
+	return msgpack.Marshal(p)
+}
+
+// Pong is the response message sent by the server after receiving a
+// Ping from the client.  A Pong concludes the handshake.
+type Pong struct {
+	_msgpack           struct{} `msgpack:",as_array"`
+	MessageType        string
+	AuthResult         bool
+	Reason             string
+	ServerHostname     string
+	SharedKeyHexDigest []byte
+}
+
+// ValidatePingDigest validates that the digest contained in the PING message
+// is valid for the client hostname (as contained in the PING).
+// Returns a non-nil error if validation fails, nil otherwise.
+func ValidatePingDigest(p *Ping, key, nonce []byte) error {
+	return validateDigest(p.SharedKeyHexDigest, key, nonce, p.SharedKeySalt, p.ClientHostname)
+}
+
+// ValidatePongDigest validates that the digest contained in the PONG message
+// is valid for the server hostname (as contained in the PONG).
+// Returns a non-nil error if validation fails, nil otherwise.
+func ValidatePongDigest(p *Pong, key, nonce, salt []byte) error {
+	return validateDigest(p.SharedKeyHexDigest, key, nonce, salt, p.ServerHostname)
+}
+
+func validateDigest(received, key, nonce, salt []byte, hostname string) error {
+	expected := computeHexDigest(salt, hostname, nonce, key)
+	if bytes.Compare(received, expected) != 0 {
+		return errors.New("No match")
+	}
+	return nil
+}
+
+func computeHexDigest(salt []byte, hostname string, nonce, sharedKey []byte) []byte {
+	h := sha512.New()
+	h.Write(salt)
+	io.WriteString(h, hostname)
+	h.Write(nonce)
+	h.Write(sharedKey)
+	sum := h.Sum(nil)
+	hexOut := make([]byte, hex.EncodedLen(len(sum)))
+	hex.Encode(hexOut, sum)
+	return hexOut
 }
--- fluent/messages_test.go
+++ fluent/messages_test.go
@@ -1,8 +1,10 @@
 package fluent_test
 
 import (
-	"fmt"
-	"os"
+	"crypto/sha512"
+	"encoding/hex"
+	"io"
+	"math/rand"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -13,18 +15,217 @@ import (
 )
 
 var _ = Describe("Messages", func() {
-	Describe("NewHelo", func() {
+	var (
+		hostname, username, password string
+		salt, nonce, key             []byte
+		authResult                   bool
+		reason                       string
+	)
+
+	BeforeEach(func() {
+		hostname = "client.host.domain"
+		username = "someuser"
+		password = "xyz123"
+
+		salt = make([]byte, 16)
+		rand.Read(salt)
+
+		nonce = make([]byte, 16)
+		rand.Read(nonce)
+
+		key = []byte(`thisisasharedkeyandmaybeitshouldbereallylong?`)
+
+		authResult = true
+	})
+
+	digest := func(salt []byte, hostname string, nonce, sharedKey []byte) []byte {
+		h := sha512.New()
+		h.Write(salt)
+		io.WriteString(h, hostname)
+		h.Write(nonce)
+		h.Write(sharedKey)
+		sum := h.Sum(nil)
+		hexOut := make([]byte, hex.EncodedLen(len(sum)))
+		hex.Encode(hexOut, sum)
+		return hexOut
+	}
+
+	Describe("PackedHelo", func() {
 		var (
 		// opts HeloOpts
 		)
 
 		It("Returns a properly-structured, msgpack-encoded HELO message", func() {
-			b, _ := NewHelo(nil)
+			b, _ := PackedHelo(nil)
 			var h Helo
 			msgpack.Unmarshal(b, &h)
-			fmt.Fprintf(os.Stderr, "VALUE IS \n%v\n", h)
-			fmt.Fprintf(os.Stderr, "ENCODED IS \n%s\n", string(b))
 			Expect(h.Options.Keepalive).To(BeTrue())
+		})
+	})
+
+	Describe("PackedPing", func() {
+		It("Returns a correctly structured and msgpack-encoded PING message", func() {
+			b, _ := PackedPing(hostname, key, salt, nonce, username, password)
+			var p Ping
+			msgpack.Unmarshal(b, &p)
+			Expect(p.MessageType).To(Equal(MSGTYPE_PING))
+			Expect(p.ClientHostname).To(Equal(hostname))
+			Expect(p.SharedKeySalt).To(Equal(salt))
+			Expect(p.SharedKeyHexDigest).To(Equal(digest(salt, hostname, nonce, key)))
+			Expect(p.Username).To(Equal(username))
+			Expect(p.Password).To(Equal(password))
+		})
+	})
+
+	Describe("PackedPong", func() {
+		It("Returns a correctly structured and msgpack-encoded PONG message", func() {
+			b, _ := PackedPong(authResult, reason, hostname, key, salt, nonce)
+			var p Pong
+			msgpack.Unmarshal(b, &p)
+			Expect(p.MessageType).To(Equal(MSGTYPE_PONG))
+			Expect(p.AuthResult).To(Equal(authResult))
+			Expect(p.Reason).To(Equal(reason))
+			Expect(p.ServerHostname).To(Equal(hostname))
+			Expect(p.SharedKeyHexDigest).To(Equal(digest(salt, hostname, nonce, key)))
+		})
+	})
+
+	Describe("ValidatePingDigest", func() {
+		var (
+			ping                               *Ping
+			digestHostname                     string
+			digestSalt, digestNonce, digestKey []byte
+		)
+
+		BeforeEach(func() {
+			digestHostname = hostname
+			digestSalt = salt
+			digestNonce = nonce
+			digestKey = key
+		})
+
+		JustBeforeEach(func() {
+			ping = &Ping{
+				MessageType:        MSGTYPE_PING,
+				ClientHostname:     hostname,
+				SharedKeySalt:      salt,
+				SharedKeyHexDigest: digest(digestSalt, digestHostname, digestNonce, digestKey),
+				Username:           username,
+				Password:           password,
+			}
+		})
+
+		It("Does not return an error", func() {
+			Expect(ValidatePingDigest(ping, key, nonce)).NotTo(HaveOccurred())
+		})
+
+		Context("When the hostname does not match the digest", func() {
+			BeforeEach(func() {
+				digestHostname = "not.the.correct.hostname"
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePingDigest(ping, key, nonce)).To(HaveOccurred())
+			})
+		})
+
+		Context("When the salt does not match the digest", func() {
+			BeforeEach(func() {
+				digestSalt = []byte(`someothersalt`)
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePingDigest(ping, key, nonce)).To(HaveOccurred())
+			})
+		})
+
+		Context("When the nonce does not match the digest", func() {
+			BeforeEach(func() {
+				digestNonce = []byte(`someothernonce`)
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePingDigest(ping, key, nonce)).To(HaveOccurred())
+			})
+		})
+
+		Context("When the key does not match the digest", func() {
+			BeforeEach(func() {
+				digestKey = []byte(`someotherkey`)
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePingDigest(ping, key, nonce)).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("ValidatePongDigest", func() {
+		var (
+			pong                               *Pong
+			digestHostname                     string
+			digestSalt, digestNonce, digestKey []byte
+		)
+
+		BeforeEach(func() {
+			digestHostname = hostname
+			digestSalt = salt
+			digestNonce = nonce
+			digestKey = key
+		})
+
+		JustBeforeEach(func() {
+			pong = &Pong{
+				MessageType:        MSGTYPE_PONG,
+				AuthResult:         authResult,
+				Reason:             reason,
+				ServerHostname:     hostname,
+				SharedKeyHexDigest: digest(digestSalt, digestHostname, digestNonce, digestKey),
+			}
+		})
+
+		It("Does not return an error", func() {
+			Expect(ValidatePongDigest(pong, key, nonce, salt)).NotTo(HaveOccurred())
+		})
+
+		Context("When the hostname does not match the digest", func() {
+			BeforeEach(func() {
+				digestHostname = "not.the.correct.hostname"
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePongDigest(pong, key, nonce, salt)).To(HaveOccurred())
+			})
+		})
+
+		Context("When the salt does not match the digest", func() {
+			BeforeEach(func() {
+				digestSalt = []byte(`someothersalt`)
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePongDigest(pong, key, nonce, salt)).To(HaveOccurred())
+			})
+		})
+
+		Context("When the nonce does not match the digest", func() {
+			BeforeEach(func() {
+				digestNonce = []byte(`someothernonce`)
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePongDigest(pong, key, nonce, salt)).To(HaveOccurred())
+			})
+		})
+
+		Context("When the key does not match the digest", func() {
+			BeforeEach(func() {
+				digestKey = []byte(`someotherkey`)
+			})
+
+			It("Returns an error", func() {
+				Expect(ValidatePongDigest(pong, key, nonce, salt)).To(HaveOccurred())
+			})
 		})
 	})
 })
