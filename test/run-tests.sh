67011f0c22b91a2b66b482dc01cc92fee84a450b
‘Hongqi <Hongqi.Zhang@ibm.com>
2021-09-29 11:46:23 -0400

    update

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+ go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.0
+ golangci-lint run ./...
+
+if [[ "$?" != "0" ]]; then
+  echo "golangci-lint run ./... failed..."
+  exit 1
+fi
+
 go test ./...
 if [[ "$?" != "0" ]]; then
   echo "go test ./... failed..."
