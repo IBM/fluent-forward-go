5980b6c82fbd7734d86842f397e115adb8efd9ad
ericmillin <31105612+ericmillin@users.noreply.github.com>
2022-01-21 15:13:06 -0500

    Remove Record type (#36)

--- fluent/protocol/transport.go
+++ fluent/protocol/transport.go
@@ -96,8 +96,6 @@ func (et *EventTime) UnmarshalBinary(timeBytes []byte) error {
 	return nil
 }
 
-type Record map[string]interface{}
-
 // EntryExt is the basic representation of an individual event, but using the
 // msgpack extension format for the timestamp.
 //msgp:tuple EntryExt
@@ -151,7 +149,7 @@ type Entry struct {
 	// Timestamp can contain the timestamp in either seconds or nanoseconds
 	Timestamp int64
 	// Record is the actual event record.
-	Record Record
+	Record interface{}
 }
 
 type MessageOptions struct {
