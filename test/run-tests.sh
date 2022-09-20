2b327a78142ba4dc6999f429d6747762cdf93728
Jeffery Chauvin <chauvinj@us.ibm.com>
2022-09-20 13:24:32 -0500

    Adding an exclude of typecheck in the linter to circumvent travis issues
    Signed-off-by: Chris Wolf <christopher.wolf@ibm.com>
    Signed-off-by: Jeff Chauvin <chauvinj@us.ibm.com>

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.0
-golangci-lint run ./...
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b ${GOPATH}/bin
+${GOPATH}/bin/golangci-lint run ./...
 
 if [[ "$?" != "0" ]]; then
   echo "golangci-lint run ./... failed..."
-  # exit 1
+  exit 1
 fi
 
 go test ./...
